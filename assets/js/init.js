803c6bf9ae7460d3eb00a73e19d7fe0f96434fbb
TGHeadle1371 <tgheadle@gmail.com>
2019-01-25 19:07:08 -0700

    Final Styles

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,9 +1,12 @@
-$(document).ready(function() {
+// On ready
+$(document).ready(function () {
+    // Floating Action Button - left - on click only
     $(".fixed-action-btn").floatingActionButton({
         direction: "left",
         hoverEnabled: false
     });
-
+    // Parallax call
     $(".parallax").parallax();
+    // Sidenav call
     $(".sidenav").sidenav();
 });
